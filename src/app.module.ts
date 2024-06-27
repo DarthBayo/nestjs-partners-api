708f62c54786b937c610b208c70304a35a951e00
Giovani <giovani@pop-os.localdomain>
2024-06-26 22:16:47 -0300

    Created crud for spots

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { EventController } from './event/event.controller';
 import { EventsModule } from './events/events.module';
 import { PrismaService } from './prisma/prisma.service';
+import { SpotsModule } from './spots/spots.module';
 
 @Module({
-  imports: [EventsModule],
+  imports: [EventsModule, SpotsModule],
   controllers: [AppController, EventController],
   providers: [AppService, PrismaService],
 })
